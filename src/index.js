69c78e1d7bbe259b6c4c1be2f1009a573cbcddc5
Christian Yungtay <christianyungtay@poli.ufrj.br>
2021-09-01 12:34:44 -0300

    Implemented the function to check if a number is happy

--- src/index.js
+++ src/index.js
@@ -15,3 +15,18 @@ function isLucky(number) {
 
   return numbersFiltred.includes(number) ? "Sortudo" : "Não-Sortudo";
 }
+
+function isHappy(number, maxIteration = 100) {
+  if (maxIteration < 1) return "Não-Feliz";
+
+  let sum = 0;
+
+  const numberAsString = "" + number;
+  const digits = numberAsString.split("");
+
+  digits.forEach((digit) => {
+    sum = sum + digit ** 2;
+  });
+
+  return sum === 1 ? "Feliz" : isHappy(sum, maxIteration - 1);
+}
