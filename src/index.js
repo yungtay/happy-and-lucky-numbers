af325d558075cebe4637981c4ce752a4f36e1e23
Christian Yungtay <christianyungtay@poli.ufrj.br>
2021-09-01 12:33:32 -0300

    Implemented the function to check if a number is lucky

---
+++ src/index.js
@@ -0,0 +1,17 @@
+function isLucky(number) {
+  let numbersFiltred;
+  numbersFiltred = Array.from({ length: number * 2 }, (_, i) => i + 1);
+
+  let i = 1;
+  while (i < numbersFiltred.length) {
+    let k = numbersFiltred[i];
+
+    numbersFiltred = numbersFiltred.filter((number, index) => {
+      return (index + 1) % numbersFiltred[i] !== 0;
+    });
+
+    if (k === numbersFiltred[i]) i++;
+  }
+
+  return numbersFiltred.includes(number) ? "Sortudo" : "Não-Sortudo";
+}
